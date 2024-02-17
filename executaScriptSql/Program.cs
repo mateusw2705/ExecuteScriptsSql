6e4a2417efc4d27cfd43f7c22bfd7195e40a2885
mateusw2705 <mateuswagner2705@gmail.com>
2024-02-17 10:13:23 -0300

    Adicionar arquivos de projeto.

---
+++ executaScriptSql/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+
+namespace ExecuteSQLScriptsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=DESKTOP-61PN5C8\\SQLEXPRESS;Initial Catalog=EasyNFe;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True"; // Substitua pela sua string de conexão
+            string scriptsFolder = @"C:\Users\w10\Desktop\Nova pasta\ScriptsInsert"; // Substitua pelo caminho da sua pasta de scripts
+
+            string[] scriptFiles = Directory.GetFiles(scriptsFolder, "*.sql");
+
+            foreach (string scriptFile in scriptFiles)
+            {
+                string script = File.ReadAllText(scriptFile);
+
+                try
+                {
+                    ExecuteScript(connectionString, script);
+                    Console.WriteLine($"Script '{Path.GetFileName(scriptFile)}' executado com sucesso!");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Erro ao executar o script '{Path.GetFileName(scriptFile)}': {ex.Message}");
+                }
+            }
+        }
+
+        static void ExecuteScript(string connectionString, string script)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(script, connection);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
